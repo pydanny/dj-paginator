fda82d496d19bc30bdd89de0da5a0226595985ec
Audrey Roy <audreyr@gmail.com>
2015-07-28 22:08:45 -0700

    Disclaimer warning about the example project secret key.

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%!b8-x4nvuecd996sd!j^v5_ye#+e!=)=7q$2&ax^)gy*rot4x'
+SECRET_KEY = 'CHANGE-ME--THIS-IS-JUST-AN-EXAMPLE-PROJECT-FOR-TESTING'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
