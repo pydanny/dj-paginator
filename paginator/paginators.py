d05b3daa1ba1704ddbeb228e7709d02599d4d0a2
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2015-09-01 20:50:14 -0700

    Fix all flake8 'E302 expected 2 blank lines, found 1' errors.

--- paginator/paginators.py
+++ paginator/paginators.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 DEFAULT_WINDOW = getattr(settings, 'PAGINATOR_DEFAULT_WINDOW', 4)
 
+
 def paginate(context, window=DEFAULT_WINDOW, hashtag=''):
     """
     Renders the ``pagination/pagination.html`` template, resulting in a
